f7646e5a2f69fc75095b03971855cf5f30c067f3
Stephen <stephen.lyssy@gmail.com>
2020-11-16 19:46:34 -0600

    Got the JPL Mars Space Images - Featured Image piece largely complete. Just need to figure out how to get it to print. Also finsished the Mars Facts piece.

--- mission_to_mars.py
+++ mission_to_mars.py
@@ -1,43 +1,75 @@
-# import pandas as pd
+import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
+import time
+# from selenium import webdriver.common.by import By
+# from selenium import webdriver.support.ui import WebDriverWait
+# from selenium import webdriver.support.import expected_conditions as EC
 
-def get_html(url, wait):
+# def get_html(url, wait):
 
-    fireFoxOptions = webdriver.FirefoxOptions()
-    fireFoxOptions.set_headless()
-    driver = webdriver.Firefox(firefox_options=fireFoxOptions)
+#     fireFoxOptions = webdriver.FirefoxOptions()
+#     fireFoxOptions.set_headless()
+#     driver = webdriver.Firefox(firefox_options=fireFoxOptions)
     
-    # driver = webdriver.Firefox()
-    driver.get(url)
-    driver.implicitly_wait(wait)
-    html = driver.page_source
-    driver.close()
+#     # driver = webdriver.Firefox()
+#     driver.get(url)
+#     driver.implicitly_wait(wait)
+#     html = driver.page_source
+#     driver.close()
 
-    return html
+#     return html
 
-url = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
-html = get_html(url, wait=5)
+# url = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
+# html = get_html(url, wait=5)
+
+# soup = BeautifulSoup(html, "html.parser")
+
+# news_titles = soup.find_all("li", class_ = "slide")
+# latest_story = news_titles[0]
+# # print(latest_story)
+# print({
+#         "article_title": latest_story.find("h3").text.strip(),
+#         "article_p": latest_story.find("div", class_ = "article_teaser_body").text.strip()
+#         })
+
+# JPL Mars Space Images - Featured Image Starts Here
+
+# def get_html(url, wait):
+#     fireFoxOptions = webdriver.FirefoxOptions()
+#     fireFoxOptions.set_headless(False)
+#     driver = webdriver.Firefox(firefox_options=fireFoxOptions)
+#     driver.get(url)
+#     driver.implicitly_wait(wait)
+#     link = driver.find_element_by_link_text("FULL IMAGE").click()
+#     # driver.implicitly_wait(2)
+#     driver.find_element_by_partial_link_text("more info").click()
+#     driver.find_element_by_class_name("main_image").click()
 
 
-soup = BeautifulSoup(html, "html.parser")
+#     # driver.close()
 
-news_titles = soup.find_all("li", class_ = "slide")
-latest_story = news_titles[0]
-# print(latest_story)
-print({
-        "article_title": latest_story.find("h3").text.strip(),
-        "article_p": latest_story.find("div", class_ = "article_teaser_body").text.strip()
-        })
+# url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
+# html = get_html(url, wait=5)
 
+# soup = BeautifulSoup(html, "html.parser")
+# print({
+#     "featured_image_url": soup.find("img", class_ = "shrinkToFit")#.find("img")["src"]
+#     })
+    
+    # for image in featured_image_url
 
-# latest_title = news_titles[0]
-# print(latest_title)
-# print(title[0] for title in news_titles)
+# # Mars Facts Starts Here
 
-# for item in news_title:
-#     title = find_all('a', {"class": "content_title"}).
-#     print(title)
-# import ssl
-# ssl._create_default_https_context = ssl._create_unverified_context
+# page = requests.get("https://space-facts.com/mars/")
+# soup = BeautifulSoup(page.content, 'html.parser')
+# tables = soup.find_all("table")
+
+# # Converting table to pandas DataFrame
+# table = tables[0]
+# tab_data = [[cell.text for cell in row.find_all(["th", "td"])] for row in table.find_all("tr")]
+# df=pd.DataFrame(tab_data)
+# print(df)
+# html_table = df.to_html(header=False, index=False)
+# print(html_table)
