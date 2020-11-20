fc159fcec97c6c38376264f02bcc3d207f60f4a5
Stephen <stephen.lyssy@gmail.com>
2020-11-19 21:40:05 -0600

    Removed packages that I was no longer using. I only had them while I was testing solutions. I shouldn't have to mess with this file anymore.

--- mission_to_mars.py
+++ mission_to_mars.py
@@ -4,9 +4,6 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
-# import numpy as np
-# import time
-# from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
 
