1da96a0d34d5d915a44e341ac92358c2c042774b
Stephen <stephen.lyssy@gmail.com>
2020-11-20 17:00:31 -0600

    Updated mars_scrape so that the hemispheres piece would return a a list of dictionaries. I originally had it set up to return a dictionary with 4 keys and 4 values.

--- mars_scrape.py
+++ mars_scrape.py
@@ -30,11 +30,11 @@ def scrape(driver):
     news_titles = soup.find_all("li", class_="slide")
 
     # Grabbing latest news title and adding it to a dict
-    latest_story = news_titles[0].find("div", class_="content_title")
+    latest_story = news_titles[1].find("div", class_="content_title")
     mars_dict["news_title"] = latest_story.text.strip()
 
     # Grabbing latest news paragraph text and adding to a dict
-    article = news_titles[0].find("div", class_="article_teaser_body")
+    article = news_titles[1].find("div", class_="article_teaser_body")
     mars_dict["news_p"] = article.text.strip()
 
 # JPL Mars Space Images - Featured Image Starts Here
@@ -63,18 +63,21 @@ def scrape(driver):
 
 # Mars Facts Starts Here
     page = requests.get("https://space-facts.com/mars/")
+    # Parsing with bs4
     soup = BeautifulSoup(page.content, "html.parser")
     tables = soup.find_all("table")
 
-    # Converting table to pandas DataFrame
+    # Indexing to grab info for the 1st table and converting table to pandas DataFrame
     table = tables[0]
     tab_data = [
         [cell.text for cell in row.find_all(["th", "td"])] for row in table.find_all("tr")
     ]
     df = pd.DataFrame(tab_data)
-    print(df)
-    mars_facts_html_table = df.to_html(header=False, index=False)
-    print(mars_facts_html_table)
+    # Renaming DF Columns
+    df.rename(columns = {0: "", 1: ""}, inplace=True)
+    #Converting DataFrame to an HTML table
+    mars_dict["mars_facts"] = df.to_html(index=False)
+    
 
 # Mars Hemispheres Starts Here
     url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
@@ -84,7 +87,7 @@ def scrape(driver):
     driver.get(url)
     driver.implicitly_wait(10)
 
-    links = driver.find_elements_by_partial_link_text("Enhanced")#.click()
+    links = driver.find_elements_by_partial_link_text("Enhanced")
     link_name_list = [link.text for link in links]
 
     # Iterating through links by using href attribute
@@ -98,9 +101,15 @@ def scrape(driver):
         image_urls = (image_src.get_attribute("src"))
         # Appending image urls for loop output to a list
         image_url_list.append(image_urls)
+# Combine link_name_list and image_url_list to a list of tuples
+    list_tuples = tuple(zip(link_name_list, image_url_list))
 
-    # Creating a dictionary by combining link_name_list and image_url_list
-    url_dict = dict(zip(link_name_list, image_url_list))
+    # Define keys to be used in dictionaries
+    keys = ("title", "img_url")
+
+    # Zipping keys to tuples for each tuple in the list of tuples
+    # Using dict comprehension to return a list of dictionaries.
+    mars_dict["mars_hemispheres"] = [dict(zip(keys, tuple))for tuple in list_tuples]
     
     return mars_dict
     driver.close()
