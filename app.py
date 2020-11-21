7e2645a69abf1c20c4f64092917872a2b0f788d8
Stephen <stephen.lyssy@gmail.com>
2020-11-21 12:52:04 -0600

    Not sure what I did here, but I tested it, and it still works.

--- app.py
+++ app.py
@@ -12,16 +12,18 @@ driver = configure_firefox_driver()
 app = Flask(__name__)
 app.config["MONGO_URI"] = os.getenv("CONN")
 
-mongo_client = PyMongo(app, connect=False)
+mongo_client = PyMongo(app)
 
 @app.route("/")
 def index():
+
     # write a statement that finds all the items in the db and sets it to a variable
-    mars_data = list(mongo_client.db.mars.find())
-    print(mars_data)
+    mars_dict = mongo_client.db.mars.find_one()
+    
 
     # render an index.html template and pass it the data you retrieved from the database
-    return render_template("index.html", mars_data=mars_data)
+    return render_template("index.html", mars_dict=mars_dict)
+
 
 @app.route("/scrape")
 def scrape_route():
