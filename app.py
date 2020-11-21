787a341e22af7a665f8209518056ff493a57184a
Stephen <stephen.lyssy@gmail.com>
2020-11-20 20:07:35 -0600

    Forgot to save app.py before my last commit.

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from mars_scrape import scrape
 from mars_scrape import configure_firefox_driver
 from flask import Flask, jsonify
-from flask_pymongo import PyMongo
+import pymongo
 import os
 
-driver = configure_firefox_driver()
 
+CONN = os.getenv()
+client = pymongo.MongoClient(CONN)
+db = client.mars
+
+
+driver = configure_firefox_driver()
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = os.getenv("CONN")
@@ -18,7 +23,7 @@ def main():
 
 @app.route("/scrape")
 def scrape_route():
-    mongo_client.db.mars.insert_one(scrape(driver)) 
+    db.mars.insert_one(scrape(driver)) 
     db.mars.drop()
     return (scrape)
 
