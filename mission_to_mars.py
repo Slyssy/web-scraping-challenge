1435ba60392102d1fdc220aea76dc6d8ba4c9374
Stephen <stephen.lyssy@gmail.com>
2020-11-21 00:42:19 -0600

    Ran black on mission to mars to clean up code.

--- mission_to_mars.py
+++ mission_to_mars.py
@@ -7,92 +7,92 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
 
+# Mars News Starts Here
+def get_html(url, wait):
+    fireFoxOptions = webdriver.FirefoxOptions()
+    fireFoxOptions.set_headless()
+    driver = webdriver.Firefox(firefox_options=fireFoxOptions)
+    driver.get(url)
+    driver.implicitly_wait(wait)
+    html = driver.page_source
+    driver.close()
 
-# # Mars News Starts Here
-# def get_html(url, wait):
-#     fireFoxOptions = webdriver.FirefoxOptions()
-#     fireFoxOptions.set_headless()
-#     driver = webdriver.Firefox(firefox_options=fireFoxOptions)
-#     driver.get(url)
-#     driver.implicitly_wait(wait)
-#     html = driver.page_source
-#     driver.close()
-
-#     return html
-
-
-# url = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
-# html = get_html(url, wait=5)
-
-# soup = BeautifulSoup(html, "html.parser")
-
-# # Finding the latest story on the page.
-# news_titles = soup.find_all("li", class_="slide")
-# latest_story = news_titles[0]
-
-# print(
-#     {
-#         "article_title": latest_story.find("h3").text.strip(),
-#         "article_p": latest_story.find(
-#             "div", class_="article_teaser_body"
-#         ).text.strip(),
-#     }
-# )
-
-# # JPL Mars Space Images - Featured Image Starts Here
-
-# def get_html(url, wait):
-#     fireFoxOptions = webdriver.FirefoxOptions()
-#     fireFoxOptions.set_headless()
-#     driver = webdriver.Firefox(firefox_options=fireFoxOptions)
-#     driver.get(url)
-#     driver.implicitly_wait(wait)
-#     driver.find_element_by_link_text("FULL IMAGE").click()
-#     driver.find_element_by_partial_link_text("more info").click()
-#     html = driver.page_source
-#     driver.close()
-#     return html
+    return html
 
 
-# url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
-# html = get_html(url, wait=10)
+url = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
+html = get_html(url, wait=5)
 
-# soup = BeautifulSoup(html, "html.parser")
+soup = BeautifulSoup(html, "html.parser")
 
-# main_image = soup.find_all("img", class_="main_image")
+# Finding the latest story on the page.
+news_titles = soup.find_all("li", class_="slide")
+latest_story = news_titles[0]
 
-# src = ""
-# for image in main_image:
-#     src = image["src"]
+print(
+    {
+        "article_title": latest_story.find("h3").text.strip(),
+        "article_p": latest_story.find(
+            "div", class_="article_teaser_body"
+        ).text.strip(),
+    }
+)
 
-# featured_image_url = "https://www.jpl.nasa.gov" + src
-
-# print(featured_image_url)
+# JPL Mars Space Images - Featured Image Starts Here
 
 
-# # Mars Facts Starts Here
+def get_html(url, wait):
+    fireFoxOptions = webdriver.FirefoxOptions()
+    fireFoxOptions.set_headless()
+    driver = webdriver.Firefox(firefox_options=fireFoxOptions)
+    driver.get(url)
+    driver.implicitly_wait(wait)
+    driver.find_element_by_link_text("FULL IMAGE").click()
+    driver.find_element_by_partial_link_text("more info").click()
+    html = driver.page_source
+    driver.close()
+    return html
 
-# page = requests.get("https://space-facts.com/mars/")
-# soup = BeautifulSoup(page.content, "html.parser")
-# tables = soup.find_all("table")
 
-# # # Converting table to pandas DataFrame
-# table = tables[0]
-# tab_data = [
-#     [cell.text for cell in row.find_all(["th", "td"])] for row in table.find_all("tr")
-# ]
-# df = pd.DataFrame(tab_data)
-# mars_facts_html_table = df.to_html(header=False, index=False)
-# print(mars_facts_html_table)
+url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
+html = get_html(url, wait=10)
 
-# # Mars Hemispheres Starts Here
+soup = BeautifulSoup(html, "html.parser")
+
+main_image = soup.find_all("img", class_="main_image")
+
+src = ""
+for image in main_image:
+    src = image["src"]
+
+featured_image_url = "https://www.jpl.nasa.gov" + src
+
+print(featured_image_url)
+
+
+# Mars Facts Starts Here
+
+page = requests.get("https://space-facts.com/mars/")
+soup = BeautifulSoup(page.content, "html.parser")
+tables = soup.find_all("table")
+
+# Converting table to pandas DataFrame
+table = tables[0]
+tab_data = [
+    [cell.text for cell in row.find_all(["th", "td"])] for row in table.find_all("tr")
+]
+df = pd.DataFrame(tab_data)
+mars_facts_html_table = df.to_html(header=False, index=False)
+print(mars_facts_html_table)
+
+# Mars Hemispheres Starts Here
 firefox_options = FirefoxOptions()
 firefox_options.add_argument("--headless")
-driver = webdriver.Firefox(options = firefox_options)
+driver = webdriver.Firefox(options=firefox_options)
 url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
 firefox_options = FirefoxOptions()
 firefox_options.add_argument("--headless")
-driver = webdriver.Firefox(options = firefox_options)
+driver = webdriver.Firefox(options=firefox_options)
 driver.get(url)
 driver.implicitly_wait(10)
 
@@ -103,12 +103,12 @@ link_name_list = [link.text for link in links]
 hrefs = [link.get_attribute("href") for link in links]
 
 # Iterating throug hrefs to extract src attribute
-image_url_list =[]
+image_url_list = []
 for href in hrefs:
     driver.get(href)
     image_src = driver.find_element_by_class_name("wide-image")
-    image_urls = (image_src.get_attribute("src"))
-    
+    image_urls = image_src.get_attribute("src")
+
     # Appending image urls for loop output to a list
     image_url_list.append(image_urls)
 
@@ -120,5 +120,5 @@ for href in hrefs:
 
     # Zipping keys to tuples for each tuple in the list of tuples
     # Using dict comprehension to return a list of dictionaries.
-    mars_hemispheres = [dict(zip(keys, tuple))for tuple in list_tuples]
-print (mars_hemispheres)
+    mars_hemispheres = [dict(zip(keys, tuple)) for tuple in list_tuples]
+print(mars_hemispheres)
