5c5fc6f6e02d9c37f23ac13d54d767c5637bbcb3
Stephen <stephen.lyssy@gmail.com>
2020-11-21 00:03:34 -0600

    Finally got the SSL issue resolved. I did this by updating my connection string. I needed to add ssl=true&ssl_cert_reqs=CERT_NONE after the ?

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 from mars_scrape import scrape
 from mars_scrape import configure_firefox_driver
-from flask import Flask, jsonify
-import pymongo
+from flask import Flask, redirect, render_template
+from flask_pymongo import PyMongo
 import os
 
 
-CONN = os.getenv()
-client = pymongo.MongoClient(CONN)
-db = client.mars
 
 
 driver = configure_firefox_driver()
@@ -15,20 +12,24 @@ driver = configure_firefox_driver()
 app = Flask(__name__)
 app.config["MONGO_URI"] = os.getenv("CONN")
 
-mongo_client = PyMongo(app)
+mongo_client = PyMongo(app, connect=False)
 
 @app.route("/")
-def main():
-    return "The app is up!"
+def index():
+    # write a statement that finds all the items in the db and sets it to a variable
+    mars_data = list(mongo_client.db.mars.find())
+    print(mars_data)
+
+    # render an index.html template and pass it the data you retrieved from the database
+    return render_template("index.html", mars_data=mars_data)
 
 @app.route("/scrape")
 def scrape_route():
-    db.mars.insert_one(scrape(driver)) 
-    db.mars.drop()
-    return (scrape)
+    mongo_client.db.mars.drop()
+    mongo_client.db.mars.insert_one(scrape(driver)) 
+    return redirect("/", code=303)
 
-# @app.route("/all")
-# def all_data()
+
 
 if __name__ == "__main__":
     app.run(debug=True)
