907b376b57f0b2f665841b8ca6d3b56faf000a19
Stephen <stephen.lyssy@gmail.com>
2020-11-20 20:06:54 -0600

    Worked on app.py program, but was unable to get it to run because of SSL error. I am going to see if I can get it to work on google cloud.

--- app.py
+++ app.py
@@ -6,20 +6,20 @@ import os
 
 driver = configure_firefox_driver()
 
-# CONN=mongodb+srv://slyssy:wnfY23SMqATuZjTD@cluster0.na11p.mongodb.net/<dbname>?retryWrites=true&w=majority
 
 app = Flask(__name__)
-# app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+app.config["MONGO_URI"] = os.getenv("CONN")
 
-# mongo_client = PyMongo(app)
+mongo_client = PyMongo(app)
 
 @app.route("/")
 def main():
     return "The app is up!"
 
 @app.route("/scrape")
-# mongo_client.db.mars.insert_many(scrape)
 def scrape_route():
+    mongo_client.db.mars.insert_one(scrape(driver)) 
+    db.mars.drop()
     return (scrape)
 
 # @app.route("/all")
